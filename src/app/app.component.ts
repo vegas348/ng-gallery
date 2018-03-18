a23ff4a7a8e65e618fa961d938e084828fa59241
vegas348 <vegas348@hotmail.com>
2018-03-18 06:57:44 +0100

    index file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Product } from './models/products';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  product: Product;
+  // tslint:disable-next-line:max-line-length
+  constructor(){
+  this.product = new Product('wimad2016', 'red wine', './resources/images/wines/red-wine/1234', ['export', 'sales', 'wine', 'red wine'], 6.50);
+}
+
 }
